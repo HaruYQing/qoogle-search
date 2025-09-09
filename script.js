9c54dcf41a65b57a96657ec636c0ceff078eff81
HaruYQing <haru61410@gmail.com>
2025-09-09 09:45:21 +0800

    feat: old search page index

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function handleSearch(event) {
   event.preventDefault();
-  window.location.assign("wu-chiu-feng");
+  window.location.assign("wu-chiu-feng/index.html");
+}
+
+function goToOldVersion(event) {
+  // event.preventDefault();
+  window.location.assign("o/index.html");
+}
+
+function handleOldSearch(event) {
+  event.preventDefault();
+  console.log(event);
+
+  // window.location.assign("wu-chiu-feng/index.html");
 }
